14971133d7489b8519b09cb459691409d58fe276
vishal1235 <130422753+1235vishal@users.noreply.github.com>
2024-04-14 01:09:53 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,40 @@ const app = express();
 
 const port = 4000;
 
+const githubData = {
+    "login": "hugh",
+    "id": 1235,
+    "node_id": "MDQ6VXNlcjEyMzU=",
+    "avatar_url": "https://avatars.githubusercontent.com/u/1235?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/hugh",
+    "html_url": "https://github.com/hugh",
+    "followers_url": "https://api.github.com/users/hugh/followers",
+    "following_url": "https://api.github.com/users/hugh/following{/other_user}",
+    "gists_url": "https://api.github.com/users/hugh/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/hugh/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/hugh/subscriptions",
+    "organizations_url": "https://api.github.com/users/hugh/orgs",
+    "repos_url": "https://api.github.com/users/hugh/repos",
+    "events_url": "https://api.github.com/users/hugh/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/hugh/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Hugh Morgenbesser",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 1,
+    "public_gists": 1,
+    "followers": 6,
+    "following": 2,
+    "created_at": "2008-02-27T22:52:17Z",
+    "updated_at": "2024-03-21T13:48:11Z"
+  }
 app.get('/',(req,resp)=>{
     resp.send("Hello world");
 })
@@ -18,6 +52,10 @@ app.get('/login',(req,resp)=>{
 
 app.get('/youtube',(req,resp)=>{
     resp.send('<h1>chai or code</h1>')
+})
+
+app.get('/github',(req,resp)=>{
+    resp.json(githubData);
 })
 
 app.listen(process.env.PORT, () =>{
